49c0383e0e44786593f8e7b1ceda61520748efeb
Craig Curtis <ccurtis@unicon.net>
2017-06-05 20:10:00 -0700

    Init commit barebonse Jasmine, zero package managers or karma test runners

---
+++ spec/ExpenseSpec.js
@@ -0,0 +1,19 @@
+describe('Expense objects', function() {
+
+
+    
+
+
+
+    it('should be of type ExpenseItem', function() {
+        var expenseItem = new ExpenseItem();
+        var expense = new Expense(expenseItem);
+        expect(expense.expenseItem).toBe(expenseItem);
+    });
+
+
+
+
+
+
+});
